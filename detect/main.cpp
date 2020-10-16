a67e6a20b6c2c8c2444cf81b4a6131a88a1ebe95
keisukeYamagishi <jam330257@gmail.com>
2020-10-16 14:43:54 +0900

    Initial Commit

---
+++ detect/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  detect
+//
+//  Created by keisuke yamagishi on 2020/10/16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
